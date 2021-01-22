98cae3d4a3bcf509050df677a4b96df3bc5bb8bf
mheyn95 <mheyn95@gmail.com>
2021-01-22 15:13:17 -0700

    added intern tests and methods

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,13 +2,13 @@ const { expect } = require("@jest/globals");
 const Engineer = require("../lib/Engineer");
 
 test("creates a Engineer object", () => {
-  const engineer = new Engineer("Dave", 5, "test@test.com");
+  const engineer = new Engineer("Dave", 5, "test@test.com", "test@github");
 
   expect(engineer.name).toBe("Dave");
 });
 
 test("run getName function", () => {
-  const engineer = new Engineer("Dave", 5, "test@test.com");
+  const engineer = new Engineer("Dave", 5, "test@test.com", "test@github");
 
   expect(engineer.getName()).toBe("Dave");
 });
