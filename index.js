efda7b0fa0f5d62f6d190f469392200aebd5887e
mheyn95 <mheyn95@gmail.com>
2021-01-23 13:57:06 -0700

    initial prompt logic in place

--- index.js
+++ index.js
@@ -1,5 +1,214 @@
-const Employee = require("../lib/Employee");
-const Manager = require("../lib/Manager");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+
+const promptManager = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the team manager's name? (Required)",
+        validate: (name) => {
+          if (name) {
+            return true;
+          } else {
+            console.log("Please enter manager's name!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "employeeId",
+        message: "Enter manager's employee ID (Required)",
+        validate: (employeeId) => {
+          if (employeeId) {
+            return true;
+          } else {
+            console.log("Please enter the managers employee ID!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter manager's email address (Required)",
+        validate: (email) => {
+          if (email) {
+            return true;
+          } else {
+            console.log("Please enter the managers email address!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "phone",
+        message: "Enter office phone number (Required)",
+        validate: (phone) => {
+          if (phone) {
+            return true;
+          } else {
+            console.log("Please enter the office phone number!");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((data) => {
+      const manager = new Manager(
+        data.name,
+        data.employeeId,
+        data.email,
+        data.phone
+      );
+      console.log(manager);
+    });
+};
+
+const promptIntern = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "internName",
+      message: "What is the intern's name? (Required)",
+      validate: (internName) => {
+        if (internName) {
+          return true;
+        } else {
+          console.log("Please enter intern's name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "employeeId",
+      message: "Enter intern's employee ID (Required)",
+      validate: (employeeId) => {
+        if (employeeId) {
+          return true;
+        } else {
+          console.log("Please enter the interns employee ID!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter intern's email address (Required)",
+      validate: (email) => {
+        if (email) {
+          return true;
+        } else {
+          console.log("Please enter the interns email address!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Enter intern's school (Required)",
+      validate: (school) => {
+        if (school) {
+          return true;
+        } else {
+          console.log("Please enter the interns school!");
+          return false;
+        }
+      },
+    },
+  ]);
+};
+
+const promptEngineer = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "engineerName",
+      message: "What is the engineer's name? (Required)",
+      validate: (engineerName) => {
+        if (engineerName) {
+          return true;
+        } else {
+          console.log("Please enter engineer's name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "employeeId",
+      message: "Enter engineer's employee ID (Required)",
+      validate: (employeeId) => {
+        if (employeeId) {
+          return true;
+        } else {
+          console.log("Please enter the engineers employee ID!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter engineer's email address (Required)",
+      validate: (email) => {
+        if (email) {
+          return true;
+        } else {
+          console.log("Please enter the engineers email address!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter engineer's github link (Required)",
+      validate: (github) => {
+        if (github) {
+          return true;
+        } else {
+          console.log("Please enter the engineers github link!");
+          return false;
+        }
+      },
+    },
+  ]);
+};
+
+const promptEmployee = () => {
+  console.log(`
+  ==================
+  Add a New Employee
+  ==================
+  `);
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "addEmployee",
+        message: "Add intern, engineer or finish? (Required)",
+        choices: "Add Intern, Add Engineer, Done Adding Team Members",
+      },
+    ])
+    .then((choiceData) => {
+      if (choiceData === "Add Intern") {
+        promptIntern();
+      } else if (choiceData === "Add Engineer") {
+        promptEngineer();
+      } else {
+        return;
+      }
+    });
+};
+
+promptManager();
---
+++ utils/html-markup.js
@@ -0,0 +1,25 @@
+module.exports = (templateData) => {
+  // destructure page data by section
+  const { projects, about, ...header } = templateData;
+
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+  
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>    
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css" integrity="sha512-UyNhw5RNpQaCai2EdC+Js0QL4RlVmiq41DkmCJsRV3ZxipG2L0HhTqIf/H9Hp8ez2EnFlkBnjRGJU2stW3Lj+w==" crossorigin="anonymous" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>    
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet"/>
+    </head>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>`;
+};
